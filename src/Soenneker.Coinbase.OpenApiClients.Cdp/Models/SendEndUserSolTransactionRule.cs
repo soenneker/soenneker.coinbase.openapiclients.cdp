3aa7dbb75a6203fdac6ebe5f30865a33945130e3
Jake Soenneker <jake@soenneker.com>
2026-03-15 17:56:43 +0000

    Automated update

--- src/Soenneker.Coinbase.OpenApiClients.Cdp/Models/SendEndUserSolTransactionRule.cs
+++ src/Soenneker.Coinbase.OpenApiClients.Cdp/Models/SendEndUserSolTransactionRule.cs
@@ -19,10 +19,10 @@ namespace Soenneker.Coinbase.OpenApiClients.Cdp.Models
         /// <summary>A schema for specifying criteria for the sendEndUserSolTransaction operation.</summary>
 #if NETSTANDARD2_1_OR_GREATER || NETCOREAPP3_1_OR_GREATER
 #nullable enable
-        public List<global::Soenneker.Coinbase.OpenApiClients.Cdp.Models.SendEndUserSolTransactionRule.Accounts>? Criteria { get; set; }
+        public List<global::Soenneker.Coinbase.OpenApiClients.Cdp.Models.SendEndUserSolTransactionRule.Policies>? Criteria { get; set; }
 #nullable restore
 #else
-        public List<global::Soenneker.Coinbase.OpenApiClients.Cdp.Models.SendEndUserSolTransactionRule.Accounts> Criteria { get; set; }
+        public List<global::Soenneker.Coinbase.OpenApiClients.Cdp.Models.SendEndUserSolTransactionRule.Policies> Criteria { get; set; }
 #endif
         /// <summary>The operation to which the rule applies. Every element of the `criteria` array must match the specified operation.</summary>
         public global::Soenneker.Coinbase.OpenApiClients.Cdp.Models.SendEndUserSolTransactionRule_operation? Operation { get; set; }
@@ -52,7 +52,7 @@ namespace Soenneker.Coinbase.OpenApiClients.Cdp.Models
             return new Dictionary<string, Action<IParseNode>>
             {
                 { "action", n => { Action = n.GetEnumValue<global::Soenneker.Coinbase.OpenApiClients.Cdp.Models.SendEndUserSolTransactionRule_action>(); } },
-                { "criteria", n => { Criteria = n.GetCollectionOfObjectValues<global::Soenneker.Coinbase.OpenApiClients.Cdp.Models.SendEndUserSolTransactionRule.Accounts>(global::Soenneker.Coinbase.OpenApiClients.Cdp.Models.SendEndUserSolTransactionRule.Accounts.CreateFromDiscriminatorValue)?.AsList(); } },
+                { "criteria", n => { Criteria = n.GetCollectionOfObjectValues<global::Soenneker.Coinbase.OpenApiClients.Cdp.Models.SendEndUserSolTransactionRule.Policies>(global::Soenneker.Coinbase.OpenApiClients.Cdp.Models.SendEndUserSolTransactionRule.Policies.CreateFromDiscriminatorValue)?.AsList(); } },
                 { "operation", n => { Operation = n.GetEnumValue<global::Soenneker.Coinbase.OpenApiClients.Cdp.Models.SendEndUserSolTransactionRule_operation>(); } },
             };
         }
@@ -64,7 +64,7 @@ namespace Soenneker.Coinbase.OpenApiClients.Cdp.Models
         {
             if(ReferenceEquals(writer, null)) throw new ArgumentNullException(nameof(writer));
             writer.WriteEnumValue<global::Soenneker.Coinbase.OpenApiClients.Cdp.Models.SendEndUserSolTransactionRule_action>("action", Action);
-            writer.WriteCollectionOfObjectValues<global::Soenneker.Coinbase.OpenApiClients.Cdp.Models.SendEndUserSolTransactionRule.Accounts>("criteria", Criteria);
+            writer.WriteCollectionOfObjectValues<global::Soenneker.Coinbase.OpenApiClients.Cdp.Models.SendEndUserSolTransactionRule.Policies>("criteria", Criteria);
             writer.WriteEnumValue<global::Soenneker.Coinbase.OpenApiClients.Cdp.Models.SendEndUserSolTransactionRule_operation>("operation", Operation);
             writer.WriteAdditionalData(AdditionalData);
         }
@@ -72,7 +72,7 @@ namespace Soenneker.Coinbase.OpenApiClients.Cdp.Models
         /// Composed type wrapper for classes <see cref="global::Soenneker.Coinbase.OpenApiClients.Cdp.Models.MintAddressCriterion"/>, <see cref="global::Soenneker.Coinbase.OpenApiClients.Cdp.Models.ProgramIdCriterion"/>, <see cref="global::Soenneker.Coinbase.OpenApiClients.Cdp.Models.SolAddressCriterion"/>, <see cref="global::Soenneker.Coinbase.OpenApiClients.Cdp.Models.SolDataCriterion"/>, <see cref="global::Soenneker.Coinbase.OpenApiClients.Cdp.Models.SolNetworkCriterion"/>, <see cref="global::Soenneker.Coinbase.OpenApiClients.Cdp.Models.SolValueCriterion"/>, <see cref="global::Soenneker.Coinbase.OpenApiClients.Cdp.Models.SplAddressCriterion"/>, <see cref="global::Soenneker.Coinbase.OpenApiClients.Cdp.Models.SplValueCriterion"/>
         /// </summary>
         [global::System.CodeDom.Compiler.GeneratedCode("Kiota", "1.0.0")]
-        public partial class Accounts : IComposedTypeWrapper, IParsable
+        public partial class Policies : IComposedTypeWrapper, IParsable
         {
             /// <summary>Composed type representation for type <see cref="global::Soenneker.Coinbase.OpenApiClients.Cdp.Models.MintAddressCriterion"/></summary>
 #if NETSTANDARD2_1_OR_GREATER || NETCOREAPP3_1_OR_GREATER
@@ -141,13 +141,13 @@ namespace Soenneker.Coinbase.OpenApiClients.Cdp.Models
             /// <summary>
             /// Creates a new instance of the appropriate class based on discriminator value
             /// </summary>
-            /// <returns>A <see cref="global::Soenneker.Coinbase.OpenApiClients.Cdp.Models.SendEndUserSolTransactionRule.Accounts"/></returns>
+            /// <returns>A <see cref="global::Soenneker.Coinbase.OpenApiClients.Cdp.Models.SendEndUserSolTransactionRule.Policies"/></returns>
             /// <param name="parseNode">The parse node to use to read the discriminator value and create the object</param>
-            public static global::Soenneker.Coinbase.OpenApiClients.Cdp.Models.SendEndUserSolTransactionRule.Accounts CreateFromDiscriminatorValue(IParseNode parseNode)
+            public static global::Soenneker.Coinbase.OpenApiClients.Cdp.Models.SendEndUserSolTransactionRule.Policies CreateFromDiscriminatorValue(IParseNode parseNode)
             {
                 if(ReferenceEquals(parseNode, null)) throw new ArgumentNullException(nameof(parseNode));
                 var mappingValue = parseNode.GetChildNode("")?.GetStringValue();
-                var result = new global::Soenneker.Coinbase.OpenApiClients.Cdp.Models.SendEndUserSolTransactionRule.Accounts();
+                var result = new global::Soenneker.Coinbase.OpenApiClients.Cdp.Models.SendEndUserSolTransactionRule.Policies();
                 if("MintAddressCriterion".Equals(mappingValue, StringComparison.OrdinalIgnoreCase))
                 {
                     result.MintAddressCriterion = new global::Soenneker.Coinbase.OpenApiClients.Cdp.Models.MintAddressCriterion();
