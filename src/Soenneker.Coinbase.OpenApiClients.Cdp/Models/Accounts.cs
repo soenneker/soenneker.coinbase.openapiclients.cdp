29f347d9375eb03bd37bfd69e38de1a39614bd77
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-04-06 17:43:21 +0000

    Automated update

--- src/Soenneker.Coinbase.OpenApiClients.Cdp/Models/Accounts.cs
+++ src/Soenneker.Coinbase.OpenApiClients.Cdp/Models/Accounts.cs
@@ -9,12 +9,12 @@ namespace Soenneker.Coinbase.OpenApiClients.Cdp.Models
 {
     [global::System.CodeDom.Compiler.GeneratedCode("Kiota", "1.0.0")]
     #pragma warning disable CS1591
-    public partial class Policies : IAdditionalDataHolder, IParsable
+    public partial class Accounts : IAdditionalDataHolder, IParsable
     #pragma warning restore CS1591
     {
         /// <summary>Stores additional data not described in the OpenAPI description found when deserializing. Can be used for serialization as well.</summary>
         public IDictionary<string, object> AdditionalData { get; set; }
-        /// <summary>A regular expression the field is matched against.</summary>
+        /// <summary>A regular expression the message is matched against. Accepts valid regular expression syntax described by [RE2](https://github.com/google/re2/wiki/Syntax).</summary>
 #if NETSTANDARD2_1_OR_GREATER || NETCOREAPP3_1_OR_GREATER
 #nullable enable
         public string? Match { get; set; }
@@ -22,24 +22,24 @@ namespace Soenneker.Coinbase.OpenApiClients.Cdp.Models
 #else
         public string Match { get; set; }
 #endif
-        /// <summary>The type of criterion to use. This should be `solMessage`.</summary>
-        public global::Soenneker.Coinbase.OpenApiClients.Cdp.Models.Policies_type? Type { get; set; }
+        /// <summary>The type of criterion to use. This should be `evmMessage`.</summary>
+        public global::Soenneker.Coinbase.OpenApiClients.Cdp.Models.Accounts_type? Type { get; set; }
         /// <summary>
-        /// Instantiates a new <see cref="global::Soenneker.Coinbase.OpenApiClients.Cdp.Models.Policies"/> and sets the default values.
+        /// Instantiates a new <see cref="global::Soenneker.Coinbase.OpenApiClients.Cdp.Models.Accounts"/> and sets the default values.
         /// </summary>
-        public Policies()
+        public Accounts()
         {
             AdditionalData = new Dictionary<string, object>();
         }
         /// <summary>
         /// Creates a new instance of the appropriate class based on discriminator value
         /// </summary>
-        /// <returns>A <see cref="global::Soenneker.Coinbase.OpenApiClients.Cdp.Models.Policies"/></returns>
+        /// <returns>A <see cref="global::Soenneker.Coinbase.OpenApiClients.Cdp.Models.Accounts"/></returns>
         /// <param name="parseNode">The parse node to use to read the discriminator value and create the object</param>
-        public static global::Soenneker.Coinbase.OpenApiClients.Cdp.Models.Policies CreateFromDiscriminatorValue(IParseNode parseNode)
+        public static global::Soenneker.Coinbase.OpenApiClients.Cdp.Models.Accounts CreateFromDiscriminatorValue(IParseNode parseNode)
         {
             if(ReferenceEquals(parseNode, null)) throw new ArgumentNullException(nameof(parseNode));
-            return new global::Soenneker.Coinbase.OpenApiClients.Cdp.Models.Policies();
+            return new global::Soenneker.Coinbase.OpenApiClients.Cdp.Models.Accounts();
         }
         /// <summary>
         /// The deserialization information for the current model
@@ -50,7 +50,7 @@ namespace Soenneker.Coinbase.OpenApiClients.Cdp.Models
             return new Dictionary<string, Action<IParseNode>>
             {
                 { "match", n => { Match = n.GetStringValue(); } },
-                { "type", n => { Type = n.GetEnumValue<global::Soenneker.Coinbase.OpenApiClients.Cdp.Models.Policies_type>(); } },
+                { "type", n => { Type = n.GetEnumValue<global::Soenneker.Coinbase.OpenApiClients.Cdp.Models.Accounts_type>(); } },
             };
         }
         /// <summary>
@@ -61,7 +61,7 @@ namespace Soenneker.Coinbase.OpenApiClients.Cdp.Models
         {
             if(ReferenceEquals(writer, null)) throw new ArgumentNullException(nameof(writer));
             writer.WriteStringValue("match", Match);
-            writer.WriteEnumValue<global::Soenneker.Coinbase.OpenApiClients.Cdp.Models.Policies_type>("type", Type);
+            writer.WriteEnumValue<global::Soenneker.Coinbase.OpenApiClients.Cdp.Models.Accounts_type>("type", Type);
             writer.WriteAdditionalData(AdditionalData);
         }
     }
