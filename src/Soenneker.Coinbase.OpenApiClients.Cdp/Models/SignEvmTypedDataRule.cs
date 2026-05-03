95878596a23d71d80b662db3cd9d02512d3f3f72
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-05-03 17:46:14 +0000

    Automated update

--- src/Soenneker.Coinbase.OpenApiClients.Cdp/Models/SignEvmTypedDataRule.cs
+++ src/Soenneker.Coinbase.OpenApiClients.Cdp/Models/SignEvmTypedDataRule.cs
@@ -19,10 +19,10 @@ namespace Soenneker.Coinbase.OpenApiClients.Cdp.Models
         /// <summary>A schema for specifying criteria for the SignEvmTypedData operation.</summary>
 #if NETSTANDARD2_1_OR_GREATER || NETCOREAPP3_1_OR_GREATER
 #nullable enable
-        public List<global::Soenneker.Coinbase.OpenApiClients.Cdp.Models.SignEvmTypedDataRule.Accounts>? Criteria { get; set; }
+        public List<global::Soenneker.Coinbase.OpenApiClients.Cdp.Models.SignEvmTypedDataRule.Policies>? Criteria { get; set; }
 #nullable restore
 #else
-        public List<global::Soenneker.Coinbase.OpenApiClients.Cdp.Models.SignEvmTypedDataRule.Accounts> Criteria { get; set; }
+        public List<global::Soenneker.Coinbase.OpenApiClients.Cdp.Models.SignEvmTypedDataRule.Policies> Criteria { get; set; }
 #endif
         /// <summary>The operation to which the rule applies. Every element of the `criteria` array must match the specified operation.</summary>
         public global::Soenneker.Coinbase.OpenApiClients.Cdp.Models.SignEvmTypedDataRule_operation? Operation { get; set; }
@@ -52,7 +52,7 @@ namespace Soenneker.Coinbase.OpenApiClients.Cdp.Models
             return new Dictionary<string, Action<IParseNode>>
             {
                 { "action", n => { Action = n.GetEnumValue<global::Soenneker.Coinbase.OpenApiClients.Cdp.Models.SignEvmTypedDataRule_action>(); } },
-                { "criteria", n => { Criteria = n.GetCollectionOfObjectValues<global::Soenneker.Coinbase.OpenApiClients.Cdp.Models.SignEvmTypedDataRule.Accounts>(global::Soenneker.Coinbase.OpenApiClients.Cdp.Models.SignEvmTypedDataRule.Accounts.CreateFromDiscriminatorValue)?.AsList(); } },
+                { "criteria", n => { Criteria = n.GetCollectionOfObjectValues<global::Soenneker.Coinbase.OpenApiClients.Cdp.Models.SignEvmTypedDataRule.Policies>(global::Soenneker.Coinbase.OpenApiClients.Cdp.Models.SignEvmTypedDataRule.Policies.CreateFromDiscriminatorValue)?.AsList(); } },
                 { "operation", n => { Operation = n.GetEnumValue<global::Soenneker.Coinbase.OpenApiClients.Cdp.Models.SignEvmTypedDataRule_operation>(); } },
             };
         }
@@ -64,7 +64,7 @@ namespace Soenneker.Coinbase.OpenApiClients.Cdp.Models
         {
             if(ReferenceEquals(writer, null)) throw new ArgumentNullException(nameof(writer));
             writer.WriteEnumValue<global::Soenneker.Coinbase.OpenApiClients.Cdp.Models.SignEvmTypedDataRule_action>("action", Action);
-            writer.WriteCollectionOfObjectValues<global::Soenneker.Coinbase.OpenApiClients.Cdp.Models.SignEvmTypedDataRule.Accounts>("criteria", Criteria);
+            writer.WriteCollectionOfObjectValues<global::Soenneker.Coinbase.OpenApiClients.Cdp.Models.SignEvmTypedDataRule.Policies>("criteria", Criteria);
             writer.WriteEnumValue<global::Soenneker.Coinbase.OpenApiClients.Cdp.Models.SignEvmTypedDataRule_operation>("operation", Operation);
             writer.WriteAdditionalData(AdditionalData);
         }
@@ -72,7 +72,7 @@ namespace Soenneker.Coinbase.OpenApiClients.Cdp.Models
         /// Composed type wrapper for classes <see cref="global::Soenneker.Coinbase.OpenApiClients.Cdp.Models.SignEvmTypedDataFieldCriterion"/>, <see cref="global::Soenneker.Coinbase.OpenApiClients.Cdp.Models.SignEvmTypedDataVerifyingContractCriterion"/>
         /// </summary>
         [global::System.CodeDom.Compiler.GeneratedCode("Kiota", "1.0.0")]
-        public partial class Accounts : IComposedTypeWrapper, IParsable
+        public partial class Policies : IComposedTypeWrapper, IParsable
         {
             /// <summary>Composed type representation for type <see cref="global::Soenneker.Coinbase.OpenApiClients.Cdp.Models.SignEvmTypedDataFieldCriterion"/></summary>
 #if NETSTANDARD2_1_OR_GREATER || NETCOREAPP3_1_OR_GREATER
@@ -93,13 +93,13 @@ namespace Soenneker.Coinbase.OpenApiClients.Cdp.Models
             /// <summary>
             /// Creates a new instance of the appropriate class based on discriminator value
             /// </summary>
-            /// <returns>A <see cref="global::Soenneker.Coinbase.OpenApiClients.Cdp.Models.SignEvmTypedDataRule.Accounts"/></returns>
+            /// <returns>A <see cref="global::Soenneker.Coinbase.OpenApiClients.Cdp.Models.SignEvmTypedDataRule.Policies"/></returns>
             /// <param name="parseNode">The parse node to use to read the discriminator value and create the object</param>
-            public static global::Soenneker.Coinbase.OpenApiClients.Cdp.Models.SignEvmTypedDataRule.Accounts CreateFromDiscriminatorValue(IParseNode parseNode)
+            public static global::Soenneker.Coinbase.OpenApiClients.Cdp.Models.SignEvmTypedDataRule.Policies CreateFromDiscriminatorValue(IParseNode parseNode)
             {
                 if(ReferenceEquals(parseNode, null)) throw new ArgumentNullException(nameof(parseNode));
                 var mappingValue = parseNode.GetChildNode("")?.GetStringValue();
-                var result = new global::Soenneker.Coinbase.OpenApiClients.Cdp.Models.SignEvmTypedDataRule.Accounts();
+                var result = new global::Soenneker.Coinbase.OpenApiClients.Cdp.Models.SignEvmTypedDataRule.Policies();
                 if("SignEvmTypedDataFieldCriterion".Equals(mappingValue, StringComparison.OrdinalIgnoreCase))
                 {
                     result.SignEvmTypedDataFieldCriterion = new global::Soenneker.Coinbase.OpenApiClients.Cdp.Models.SignEvmTypedDataFieldCriterion();
